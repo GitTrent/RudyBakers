e06c2c8dfab5589064881e857a4b1f64cbdff927
GitTrent <trent@rudisill.net>
2015-10-19 00:42:21 -0500

    inital commint

---
+++ RudyBakers.Data/PaymentMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RudyBakers.Data
+{
+	class PaymentMethod
+	{
+		public string Number { get; set; }
+		public string ExpireDate { get; set; }
+		public string CVV { get; set; }
+		public Address Address { get; set; }
+	}
+}
---
+++ RudyBakers.Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RudyBakers.Data
+{
+    public class User
+    {
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public IDictionary<string, Address> Addresses { get; set; }
+		public IDictionary<string, PaymentMethod> PaymentMethods { get; set; }
+    }
+}
