dfd3d8d2a50ad2feb909447b604f5e45c62102f4
Trent Rudisill <trent.rudisill@live.com>
2015-12-15 09:25:54 -0600

    inital check in

---
+++ RudyBakers.Data/Model/FoodItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RudyBakers.Data.Model
+{
+	public class FoodItem : IItem
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string ShortDescription { get; set; }
+		public string LongDescription { get; set; }
+		public virtual ICollection<Price> Prices { get; set; }
+		public virtual ICollection<HealthInfo> HealthInfos { get; set; }
+
+		public FoodItem()
+		{
+			this.Prices = new HashSet<Price>();
+			this.HealthInfos = new HashSet<HealthInfo>();
+		}
+	
+}
+}
